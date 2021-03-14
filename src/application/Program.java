e14aa5d3104052fb772972e59f9b41d2bc5f03a1
ThoniMiguel <wanok8000@outlook.com>
2021-03-14 15:48:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,19 @@ package application;
 import db.DB;
 import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = DB.getConnection();
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+        Seller seller = new Seller(2,"Thoni", "thoni@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
