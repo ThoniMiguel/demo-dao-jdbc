d2ca10168ffb22ffc1f472fe8816eb5acc288793
ThoniMiguel <wanok8000@outlook.com>
2021-03-14 16:28:35 -0300

    findby implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-        Seller seller = new Seller(2,"Thoni", "thoni@gmail.com", new Date(), 3000.0, obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
